e0da7b2ca47192f7b46b01779a09892b3768f7be
Jose <jrleon90@gmail.com>
2018-07-31 15:30:07 -0500

    :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import SearchBar from './containers/search_bar';
+import WeatherList from './containers/weather_list';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <WeatherList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from '../src/reducers';
+
+import registerServiceWorker from './registerServiceWorker';
+
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+registerServiceWorker();
